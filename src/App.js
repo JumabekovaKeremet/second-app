e9e0ec2f9d578446863249d8fe4120de2e97ddac
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-03-10 15:57:32 +0600

    changed file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from "react";
-// import Switch from "./components/Switch";
-// import Counter from "./components/Counter";
-// import Input from "./components/Input";
-// import Text from "./components/Text";
-import Massive from "./components/Massive";
-import Counter from "./components/Counter"
 
-function Switch () {
+import "./App.css";
+import PersonList from "./components/PersonList/PersonList";
+function App() {
+  const persons = [
+    { name: "Elon Baike", age: 47, hobby: "Ice skating, Eating donuts, Dancing under the moon" },
+    { name: "Bill Baike", age: 50, hobby: "Saving the world" }
+  ];
   return (
-      <div className="App">
-          <Counter />
-          <Massive />
-      </div>
-  )
+    <div className="App">
+      <PersonList persons={persons} />
+    </div>
+  );
 }
 export default App;
